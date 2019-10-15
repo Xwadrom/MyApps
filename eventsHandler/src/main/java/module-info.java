4bf18cd8a866579316b41dd0ea968ad0dc312347
Robert Osuch <xwadrom@gmail.com>
2019-10-15 19:31:12 +0200

    del

--- eventsHandler/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module eventsHandler {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    exports com.github.xwadrom.fxaction.main;
-    opens com.github.xwadrom.fxaction.controller to javafx.fxml;
-}
