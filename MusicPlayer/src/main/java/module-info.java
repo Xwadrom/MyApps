928d35b0b5f93cb513474cd758caf66cc3e753df
Robert Osuch <xwadrom@gmail.com>
2019-11-04 19:48:51 +0100

    m

--- MusicPlayer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module musicplayer {
-        requires javafx.graphics;
-        requires javafx.controls;
-        requires javafx.fxml;
-
-        exports com.github.xwadrom.musicplayer.main to javafx.graphics;
-        opens com.github.xwadrom.musicplayer.controller to javafx.fxml;
-        }
